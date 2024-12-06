0369930478c36ecc4d10ee20663d24b1929dd244
Łukasz Szczygielski <159384856+Cooki3GOD@users.noreply.github.com>
2024-12-06 09:51:46 +0100

    Added function that marks an element as complete

--- script.js
+++ script.js
@@ -8,8 +8,24 @@ addBtn.addEventListener("click", () => {
         li.innerHTML = `${inputField.value} <p>X</p>`;
         list.appendChild(li);
         inputField.value = "";
+        li.addEventListener("click", () => {
+            if(li.classList.contains("done")) {
+                li.classList.remove("done");
+            }
+            else {
+                li.classList.add("done");
+            }
+        })
+        const remove = document.querySelectorAll("li p");
+        remove.forEach((btn) => {
+            btn.addEventListener("click", () => {
+                
+            })
+        })
+
     }
     else {
         alert("You need to type something!");
     }
+    
 })
